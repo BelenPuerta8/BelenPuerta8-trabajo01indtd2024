213e8fc52ec7cbe1efbb3d4bc566b63e421ccd3d
BelenPuerta8 <belenpuertagonzalez2003@gmail.com>
2024-10-08 16:14:43 +0200

    métodos problema 1

--- Resolución.R
+++ Resolución.R
@@ -19,17 +19,100 @@ datos = crea.tablaX(c(7, 3, 5,
 datos
 
 
-### CRITERIO PESIMISTA (WALD) ### 
+##### CRITERIO PESIMISTA (WALD) #####
 ## Caso favorable ##
 p1_pes_FAV = criterio.Wald(datos,T)
 p1_pes_FAV
-names(p1_pes_FAV$AlternativaOptima) # La mejor alternativa es d3
+names(p1_pes_FAV$AlternativaOptima) # La mejor alternativa es d1
 
 
 ## Caso desfavorable ##
 p1_pes_DESFAV = criterio.Wald(datos,F)
 p1_pes_DESFAV
-names(p1_pes_DESFAV$AlternativaOptima) # La mejor alternativa es d3
+names(p1_pes_DESFAV$AlternativaOptima) # La mejor alternativa es d2
+
+
+
+
+
+##### CRITERIO OPTIMISTA #####
+## Caso favorable ##
+p1_opt_FAV = criterio.Optimista(datos,T)
+p1_opt_FAV
+names(p1_opt_FAV$AlternativaOptima) # La mejor alternativa es d3
+
+
+## Caso desfavorable ##
+p1_opt_DESFAV = criterio.Optimista(datos,F)
+p1_opt_DESFAV
+names(p1_opt_DESFAV$AlternativaOptima) # La mejor alternativa es d4
+
+
+
+
+
+##### CRITERIO DE HURWICZ #####
+## Caso favorable ##
+p1_hur_FAV = criterio.Hurwicz(datos,T)
+p1_hur_FAV
+names(p1_hur_FAV$AlternativaOptima) # La mejor alternativa es d3
+
+
+## Caso desfavorable ##
+p1_hur_DESFAV = criterio.Hurwicz(datos,F)
+p1_hur_DESFAV
+names(p1_hur_DESFAV$AlternativaOptima) # La mejor alternativa es d1
+
+
+
+
+
+##### CRITERIO DE SAVAGE #####
+## Caso favorable ##
+p1_sav_FAV = criterio.Savage(datos,T)
+p1_sav_FAV
+names(p1_sav_FAV$AlternativaOptima) # La mejor alternativa es d1
+
+
+## Caso desfavorable ##
+p1_sav_DESFAV = criterio.Savage(datos,F)
+p1_sav_DESFAV
+names(p1_sav_DESFAV$AlternativaOptima) # La mejor alternativa es d1 y d2
+
+
+
+
+
+##### CRITERIO DE LAPLACE #####
+## Caso favorable ##
+p1_lap_FAV = criterio.Laplace(datos,T)
+p1_lap_FAV
+names(p1_lap_FAV$AlternativaOptima) # La mejor alternativa es d1
+
+
+## Caso desfavorable ##
+p1_lap_DESFAV = criterio.Laplace(datos,F)
+p1_lap_DESFAV
+names(p1_lap_DESFAV$AlternativaOptima) # La mejor alternativa es d4
+
+
+
+
+
+
+##### CRITERIO DEL PUNTO IDEAL #####
+## Caso favorable ##
+p1_pid_FAV = criterio.PuntoIdeal(datos,T)
+p1_pid_FAV
+names(p1_pid_FAV$AlternativaOptima) # La mejor alternativa es d1
+
+
+## Caso desfavorable ##
+p1_pid_DESFAV = criterio.PuntoIdeal(datos,F)
+p1_pid_DESFAV
+names(p1_pid_DESFAV$AlternativaOptima) # La mejor alternativa es d2
+
+
 
 
 
