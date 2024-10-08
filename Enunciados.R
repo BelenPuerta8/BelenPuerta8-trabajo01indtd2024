8ca58c7f392d6adf07e5c5d4e5dcd76bc6674273
BelenPuerta8 <belenpuertagonzalez2003@gmail.com>
2024-10-08 19:37:29 +0200

    Enunciados problemas propuestos

--- Enunciados.R
+++ Enunciados.R
@@ -1,30 +1,66 @@
 ####################### PROBLEMA 1 ###########################
-# Aplicar los criterios de decisión bajo incertidumbre a la 
-# siguiente matriz de valores: 
-#
-####### e1 ### e2 ### e3 #
-# d1  # 25  #  15  #  5  #
-# d2  # 30  #  12  #  2  #
-# d3  # 20  #  20  #  10 #
-##########################
+# Aplicar los criterios de decisión bajo incertidumbre a la  #
+# siguiente matriz de valores:                               #
+#                                                            #
+#            ################################                #
+#            #########  e1  #  e2  #  e3 ####                #         
+#            ################################                #
+#            ## d1  ##  5   #  3   #  -1  ###                #
+#            ## d2  ##  12  #  6   #  6   ###                #
+#            ## d3  ##  10  #  4   #  8   ###                #
+#            ## d4  ##  13  #  4   #  1   ###                #
+#            ## d5  ##  5   #  8   #  10  ###                #
+#            ################################                #
+#                                                            #
+#  Resolver primero para el caso FAVORABLE (beneficios) y,   #
+#  posteriormente, para el caso DESFAVORABLE (costos).       #
+#                                                            #
+##############################################################
 
 
 
 
 ####################### PROBLEMA 2 ###########################
-# Un inversionista está evaluando invertir en uno de los cuatro 
-# siguientes activos financieros bajo tres posibles escenarios 
-# del mercado: mercado alcista, mercado estable, y mercado 
-# bajista. 
-#
-# Los rendimientos proyectados (en millones de euros) para los 
-# tipos de mercados nombrados anteriormente son los siguientes:
-# ACCIONES TECNOLÓGICAS: 15, 7, 1
-# BONOS DEL GOBIERNO: 5, 5, 5
-# FONDOS DE BIENES RAÍCES: 10, 6, 2
-# CRIPTOMONEDAS: 25, 3, 1
-#
-# ¿Cuál sería la opción que usted le recomendaría al inversionista?
-
-
-
+# Un inversionista ha decidido analizar diversas opciones de 
+# inversión, ya que busca maximizar sus ganancias bajo distintos 
+# escenarios del mercado. Tras meses de estudio y consultas con 
+# expertos financieros, ha reducido sus opciones a cuatro activos 
+# financieros que, según su análisis, presentan distintas 
+# oportunidades de rendimiento. 
+# 
+# Ha identificado tres posibles escenarios del mercado: mercado 
+# alcista, en el que se espera un crecimiento considerable de la 
+# economía; mercado estable, donde los rendimientos se mantendrán 
+# sin grandes cambios; y mercado bajista, en el que la economía 
+# enfrentará dificultades.
+# 
+# Con base en las proyecciones actuales, los rendimientos 
+# estimados para cada activo bajo estos escenarios, expresados en 
+# millones de euros, son los siguientes:
+#   
+# *Acciones tecnológicas: en un escenario alcista generarían 15 
+#                         millones, en un mercado estable producirían 
+#                         7 millones, y en un mercado bajista solo 
+#                         darían 1 millón. Este tipo de activo se ha 
+#                         caracterizadompor su enorme potencial en épocas 
+#                         de bonanza.
+# 
+# *Bonos del gobierno: se proyecta que generen un rendimiento 
+#                      constante de 5 millones sin importar el estado 
+#                      del mercado. Se consideran una opción de bajo 
+#                      riesgo, especialmente en tiempos de incertidumbre, 
+#                      debido a su estabilidad.
+# 
+# *Fondos de bienes raíces: bajo un mercado alcista podrían proporcionar 
+#                           un rendimiento de 10 millones, en un mercado 
+#                           estable 6 millones, y en un escenario bajista 
+#                           2 millones. 
+# 
+# * Criptomonedas: en un mercado alcista su rentabilidad podría alcanzar 
+#                  los 25 millones, en un mercado estable solo generarían 
+#                  3 millones, y en un mercado bajista apenas 1 millón. 
+#                  Este activo es conocido por su alta volatilidad.
+# 
+# Dado este panorama y los rendimientos proyectados, ¿cuál sería la opción 
+# más adecuada que recomendarías al inversionista?
+######################################################################
